1eeda662bf1adc8c056f8251748ecb3fb8229e5a
balatamilmani <btamilma.ptr@gmail.com>
2017-11-07 12:43:43 -0500

    Project structure checkin

---
+++ esclient/src/main/java/com/balatamilmani/esclient/EsclientApplication.java
@@ -0,0 +1,12 @@
+package com.balatamilmani.esclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsclientApplication.class, args);
+	}
+}
