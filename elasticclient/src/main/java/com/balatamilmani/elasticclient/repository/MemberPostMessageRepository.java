85f2ed0bd05f38accb6955996a8ac8dadc1facc4
balatamilmani <btamilma.ptr@gmail.com>
2017-11-12 00:19:13 -0500

    Sample ElasticSearch client

--- elasticclient/src/main/java/com/balatamilmani/elasticclient/repository/MemberPostMessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.balatamilmani.elasticclient.repository;
-
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-import com.balatamilmani.elasticclient.model.MemberPostMessage;
-
-public interface MemberPostMessageRepository extends ElasticsearchRepository<MemberPostMessage, String>{
-
-}
